44525a0bb267b6d7a2baf4282a66606d41dd7423
Aleksandr Butakov <anbngm@gmail.com>
2022-03-17 18:23:11 +0500

    Added attachments

--- src/test/java/tests/demoqa/RegistrationTests.java
+++ src/test/java/tests/demoqa/RegistrationTests.java
@@ -2,7 +2,9 @@ package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -30,6 +32,14 @@ public class RegistrationTests {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
     @Test
